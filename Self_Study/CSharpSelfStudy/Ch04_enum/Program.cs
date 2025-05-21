4e2dda8aedcef67936c22a3e532e0f14efd4f59c
talCSHN <yujakinasakoon@gmail.com>
2025-05-21 16:06:02 +0900

    Clone my study history from different Repository
    winapp 리포지토리에 올렸던 개인공부 기록 클론

---
+++ Self_Study/CSharpSelfStudy/Ch04_enum/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Ch04_enum
+{
+    internal class Program
+    {
+        enum Days
+        {
+            Sunday, Monday, Tuesday, Wednesday, Friday, Saturday
+        }
+        // 이런 식으로 시작 요소 값에 0이 아닌 다른 정수 지정도 가능
+        // 그 이후의 요소에 대해서도 1씩으로 자동 증가하는 것이 아닌 임의의 값 지정 가능
+        // 대신 상속받은 부모의 숫자 타입 범위에 있는 값 지정해야함
+        enum Days1
+        {
+            Sunday = 5, Monday = 10, Tuesday, Wednesday, Thursday = 15, Friday, Saturday
+        }
+        [Flags]
+        enum Days2
+        {
+            Sunday = 1, Monday = 2, Tuesday = 4, Wednesday = 8, Thursday = 16, Friday = 32, Saturday = 64
+        }
+        enum CalcType
+        {
+            Add, Minus, Multiply, Divide
+        }
+        static void Main(string[] args)
+        {
+            Days today = Days.Sunday;
+            Console.WriteLine(today);
+            int n = (int)today;
+            Console.WriteLine(n);
+            //today = (int)today; // Days 타입 변수에 int 형 값을 넣는 암시적 형변환은 불가
+            today = (Days)5;
+            Console.WriteLine(today);
+
+            Days2 workingDays = Days2.Monday | Days2.Tuesday | Days2.Wednesday | Days2.Thursday | Days2.Friday;
+            Console.WriteLine(workingDays.HasFlag(Days2.Sunday));   // Sunday를 포함하고 있는가
+            Days2 today2 = Days2.Friday;
+            Console.WriteLine(workingDays.HasFlag(today2));
+
+            Console.WriteLine(workingDays);
+
+            int Calc(CalcType opType, int operand1, int operand2)
+            {
+                switch (opType)
+                {
+                    case CalcType.Add:
+                        return operand1 + operand2;
+                    case CalcType.Minus:
+                        return operand1 - operand2;
+                    case CalcType.Multiply:
+                        return operand1 * operand2;
+                    case CalcType.Divide:
+                        return operand1 / operand2;
+                }
+                return 0;
+                
+            }
+            Calc(CalcType.Add, 10, 5);  // enum 인스턴스로 전달
+        }
+    }
+}
---
+++ Self_Study/CSharpSelfStudy/Ch04_this/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Security.Cryptography;
+
+namespace Ch04_this
+{
+    internal class Program
+    {
+        class Book
+        {
+            decimal _isbn;
+            public decimal ISBN
+            {
+                get { return this._isbn; }
+            }
+
+            public Book(decimal isbn)
+            {
+                this._isbn = isbn;
+            }
+
+            public decimal GetISBN()
+            {
+                return this.ISBN;
+            }
+
+            public void Sell()
+            {
+                Console.WriteLine("Sell: " + this.GetISBN());
+            }
+        }
+
+        // this 예약어 사용해서 생성자 내에서 다른 생성자 호출
+        class Book1
+        {
+            string title;
+            decimal isbn13;
+            string author;
+
+            public Book1(string title) : this(title, 0)
+            {
+
+            }
+            public Book1(string title, decimal isbn13) : this(title, isbn13, string.Empty)
+            {
+
+            }
+            public Book1(string title, decimal isbn13, string author)
+            {
+                this.title = title;
+                this.isbn13 = isbn13;
+                this.author = author;
+            }
+            public Book1() : this(string.Empty, 0, string.Empty)
+            {
+
+            }
+        }
+
+        class Book2
+        {
+            string title;
+            static int count;
+
+            public Book2(string title)  // 인스턴스 생성자
+            {
+                this.title = title; // this로 인스턴스 필드 식별 가능
+                this.Open();        // this로 인스턴스 메서드 식별 가능
+                Increment();        // static 메서드 사용 가능
+            }
+
+            static void Increment() // static 메서드
+            {
+                count++;    // static 필드 사용 가능
+                // static 메서드에는 this가 없으므로 인스턴스 멤버 사용 불가능
+            }
+            public void Close()
+            {
+                Console.WriteLine(this.title + "책 덮기");
+            }
+
+            public void Open()
+            {
+                Console.WriteLine(this.title + "Siuuuu");   // 인스턴스 멤버 사용 가능
+                Console.WriteLine(count);   // static 멤버 사용 가능
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
