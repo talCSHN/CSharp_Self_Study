a64361e24d45ead19af4753baee61f22431c1eb5
talCSHN <yujakinasakoon@gmail.com>
2025-06-02 19:25:46 +0900

    Update Program.cs

---
+++ Self_Study/CSharpSelfStudy/Practical_Idioms/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Practical_Idioms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 배열/리스트 초기화. 가급적 마지막 요소 뒤 콤마 붙일 것
+            var langs = new string[] { "C#", "JAVA", "C++", };
+            var nums = new List<int> { 10, 20, 30, 40, };
+        }
+    }
+}
