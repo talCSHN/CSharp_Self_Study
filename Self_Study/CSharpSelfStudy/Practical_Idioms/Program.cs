49078d699b719ae8bedf12cde0c92c647fb0ea0c
talCSHN <yujakinasakoon@gmail.com>
2025-06-04 00:22:10 +0900

    Update Program.cs

--- Self_Study/CSharpSelfStudy/Practical_Idioms/Program.cs
+++ Self_Study/CSharpSelfStudy/Practical_Idioms/Program.cs
@@ -33,6 +33,18 @@
             // List<T> 요소 처리
             var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             list.ForEach(n => Console.Write($"{n}\t"));
+            Console.WriteLine();
+            // do-while
+            bool finish;
+            do
+            {
+                finish = DoSomething();
+            } while (!finish);
+        }
+
+        private static bool DoSomething()
+        {
+            return false;
         }
     }
 }
