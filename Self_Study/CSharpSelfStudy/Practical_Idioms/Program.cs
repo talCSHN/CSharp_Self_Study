eb4401f58581283747a3009909505bfc602fb92b
talCSHN <yujakinasakoon@gmail.com>
2025-06-03 23:08:19 +0900

    CSharp Self Study Commit

--- Self_Study/CSharpSelfStudy/Practical_Idioms/Program.cs
+++ Self_Study/CSharpSelfStudy/Practical_Idioms/Program.cs
@@ -7,6 +7,14 @@
             // 배열/리스트 초기화. 가급적 마지막 요소 뒤 콤마 붙일 것
             var langs = new string[] { "C#", "JAVA", "C++", };
             var nums = new List<int> { 10, 20, 30, 40, };
+            // Dictionary 초기화
+            var dict = new Dictionary<string, string>()
+            {
+                ["ko"] = "한국어",
+                ["en"] = "영어",
+                ["jp"] = "일본어",
+            };
+            Console.WriteLine(dict["ko"]);
         }
     }
 }
