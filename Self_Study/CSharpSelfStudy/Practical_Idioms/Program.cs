41788d22c14cb7ee9082770528c1d2abb5a5ba0c
talCSHN <yujakinasakoon@gmail.com>
2025-06-04 23:30:23 +0900

    CSharp Self Study Commit

--- Self_Study/CSharpSelfStudy/Practical_Idioms/Program.cs
+++ Self_Study/CSharpSelfStudy/Practical_Idioms/Program.cs
@@ -1,12 +1,31 @@
-﻿namespace Practical_Idioms
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Practical_Idioms
 {
     public class Person
     {
         public string Name {  get; set; }
         public DateTime Birthday { get; set; }
         public string PhoneNumber {  get; set; }
+        // 읽기 전용 속성
+        public string GivenName { get; private set; }
+        public string FamilyName { get; private set; }
+        public string FullName => GivenName + " " + FamilyName;
         public Person()
         {
+        }
+    }
+    public class MySample
+    {
+        // 읽기 전용 속성이 참조형인 경우(string 제외) 참조는 수정 불가, 속성이 가리키고 있는 객체는 수정 가능
+        // 즉 List<int> 컬렉션의 내용은 수정 가능
+        public List<int> MyList { get; private set; }
+        // 더 강력한 방법. 컬렉션 자체를 수정할 수 없도록
+        public IReadOnlyList<int> MyList1 { get; private set; }
+        public MySample()
+        {
+            MyList = new List<int>() { 1, 2, 3, 4, 5 };
+            MyList1 = new List<int>() { 6, 7, 8, 9, 10 };
         }
     }
     internal class Program
@@ -39,7 +58,71 @@
             do
             {
                 finish = DoSomething();
-            } while (!finish);
+            } while (finish);
+            // 루프 중단 처리
+            var items = new List<string> { "21대", " 대선", };
+            var line = "";
+            foreach (var item in items)
+            {
+                if (line.Length + item.Length > 40)
+                {
+                    break;
+                }
+                line += item;
+            }
+            Console.WriteLine(line);
+
+            loop();
+
+            // null 조건 연산자
+            Console.WriteLine(items[1]?.ToString());
+
+            // 읽기 전용 속성
+            var obj = new MySample();
+            obj.MyList.Add(6);  // 가능
+            //obj.MyList = new List<int>();   // 빌드 오류
+
+            // 예외처리
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;  // 권장
+                //throw ex; // 권장하지 않음. 이유 : 예외의 스택 트레이스 정보 사라짐
+            }
+
+            // 외부 자원에 접근해야할 때는 가급적 using 사용 권장.
+            // 이유 : 자동으로 Dispose가 호출되어 메모리 누수 방지 가능
+
+        }
+        // 가변 인수
+        public static double Median(params double[] args)
+        {
+            var sorted = args.OrderBy(n => n).ToArray();
+            int index = sorted.Length / 2;
+            if (sorted.Length % 2 == 0)
+            {
+                return (sorted[index] + sorted[index - 1]) / 2;
+            }
+            else
+            {
+                return sorted[index];
+            }
+        }
+        public static int loop()
+        {
+            var numbers = new int[] { 1, 2, 3 };
+            foreach (var n in numbers)
+            {
+                if (n > 2)
+                {
+                    return n;
+                }
+            }
+            return -1;
         }
 
         private static bool DoSomething()
---
+++ Self_Study/CSharpSelfStudy/Practical_String/Program.cs
@@ -0,0 +1,121 @@
+﻿namespace Practical_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////////////////////////// 문자열 비교 ////////////////////////////
+            string str1 = "ABC";
+            string str2 = "abc";
+            // == 연산자 : 대소문자 구분
+            if (str1 == str2)
+            {
+                Console.WriteLine("Same");
+            }
+            else
+            {
+                Console.WriteLine("Different");
+            }
+            // String.Compare : 대소문자 구분 x 가능
+            if (String.Compare(str1, str2, ignoreCase:true) == 0)
+            {
+                Console.WriteLine("Same");
+            }
+
+            ////////////////////////// 문자열 변환 ////////////////////////////
+
+            // null, 빈 문자열 판정
+            if (String.IsNullOrEmpty(str1))
+            {
+                Console.WriteLine("Empty or Null");
+            }
+            else
+            {
+                Console.WriteLine("Exist");
+            }
+            // 빈 문자열 판정
+            if (str1 == String.Empty)
+            {
+                Console.WriteLine("Empty");
+            }
+            else
+            {
+                Console.WriteLine("Exist");
+            }
+            // null, 빈 문자열, 공백 문자열 판정
+            if (String.IsNullOrWhiteSpace(str2))
+            {
+                Console.WriteLine("null or empty or space");
+            }
+            else
+            {
+                Console.WriteLine("Exist");
+            }
+            // 지정한 부분 문자열로 시작 여부
+            if (str1.StartsWith("abc"))
+            {
+                Console.WriteLine("True");
+            }
+            // 지정한 부분 문자열로 끝남 여부
+            if (str1.EndsWith("abc"))
+            {
+                Console.WriteLine("True");
+            }
+            // 지정한 부분 문자열 포함 여부
+            if (str1.Contains("abc"))
+            {
+                Console.WriteLine("True");
+            }
+            var contains = str1.Contains('a');
+            Console.WriteLine(contains);
+            // 조건을 만족하는 문자 포함돼 있는지 여부
+            var target = "C# Programming";
+            var isExists = target.Any(c => Char.IsLower(c));
+            // 모든 문자가 조건 만족하는지 여부
+            var target1 = "123456789";
+            var isAllDigits = target1.All(c => Char.IsDigit(c));
+
+            ////////////////////////// 문자열 검색, 추출 ////////////////////////////
+            
+            // 부분 문자열 검색 후 위치 반환
+            var target2 = "Novelist=호날두;BestWork=내마위";
+            var index = target2.IndexOf("BestWork=");
+            Console.WriteLine(index);
+
+            // 문자열 일부 추출
+            var bestWork = target2.Substring(index, target2.Length - index);
+            Console.WriteLine(bestWork);
+
+            ////////////////////////// 문자열 변환 ////////////////////////////
+
+            // 문자열 앞뒤 공백 제거
+            var target3 = " 호날두 메시 ";
+            var replaced = target3.Trim();
+            Console.WriteLine(replaced);
+            // 한 쪽 공백만 제거
+            var replaced1 = target3.TrimStart();
+            var replaced2 = target3.TrimEnd();
+            Console.WriteLine(replaced1 + replaced2);
+            // 지정 위치부터 임의 개수 문자 삭제
+            var result = target2.Remove(5, 3);
+            Console.WriteLine(result);
+            // 문자열에 다른 문자열 삽입
+            var result1 = target2.Insert(2, "abc");
+            // 문자열 일부 다른 문자열로 치환
+            var replaced3 = target2.Replace("BestWork", "GOAT");
+            // 대소문자 변환
+            var replaced4 = target2.ToUpper();
+            var replaced5 = target2.ToLower();
+
+            ////////////////////////// 문자열 연결, 분할 ////////////////////////////
+            var name = "호" + "날두";
+            var name1 = "호날두";
+            name1 += ": GOAT";
+            // 지정한 구분 문자로 문자열 배열 연결
+            var people = new[] { "호날두", "메시", "벨링엄", "음바페", };
+            var seperator = ", ";
+            var result5 = String.Join(seperator, people);
+            Console.WriteLine(result5);
+        }
+    }
+}
